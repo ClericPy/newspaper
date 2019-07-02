bfbda1ac76f75915f70a2c0562542f2143f49131
clericpy <lidongone@qq.com>
2019-07-02 14:21:21 +0800

    修复全文索引检索的问题

--- newspaper/models.py
+++ newspaper/models.py
@@ -297,7 +297,18 @@ class MySQLStorage(Storage):
         limit = min((self.max_limit, int(limit)))
         offset = int(offset)
         lang = str(lang).upper()
+        extra_select_words: str = ''
 
+        if query:
+            # 带检索词的，添加上字段方便排序
+            extra_select_words = ', MATCH (`title`, `desc`, `url`) AGAINST (%s IN BOOLEAN MODE)  as relevance'
+            args.append(query)
+            where_list.append(
+                'MATCH (`title`, `desc`, `url`) AGAINST (%s in BOOLEAN MODE)')
+            args.append(query)
+        if order_by not in self.articles_table_columns and order_by != 'relevance':
+            order_by = 'ts_create'
+        order_by_sorting = f'order by {order_by} {sorting}'
         if date:
             if date == 'today':
                 date = ttime()[:10]
@@ -324,16 +335,6 @@ class MySQLStorage(Storage):
             where_list.append("`source` = %s")
             args.append(source)
             result['source'] = source
-        if query:
-            where_list.append('MATCH(`title`, `desc`, `url`) AGAINST(%s)')
-            args.append(query)
-            if order_by == 'none':
-                # 按索引相关度倒序
-                order_by_sorting = ''
-            else:
-                if order_by not in self.articles_table_columns:
-                    order_by = 'ts_create'
-                order_by_sorting = f'order by {order_by} {sorting}'
 
         if lang in {'CN', 'EN'}:
             where_list.append("`lang` = %s")
@@ -352,7 +353,7 @@ class MySQLStorage(Storage):
             where_string = 'where ' + ' and '.join(where_list)
         else:
             where_string = ''
-        sql = f"SELECT * from articles {where_string} {order_by_sorting} limit %s offset %s"
+        sql = f"SELECT *{extra_select_words} from articles {where_string} {order_by_sorting} limit %s offset %s"
         logger.info(f'fetching articles sql: {sql}, args: {args}')
         items = await self.execute(sql, args)
         result['has_more'] = 1 if len(items) > limit else 0
